f5df01773e0e2754269afef6e2dac51f9e013a2a
cristobal <c.ramos07@ufromail.cl>
2025-06-19 08:45:15 -0400

    implementación juego dados

--- src/main/java/Controlador/JuegoDados.java
+++ src/main/java/Controlador/JuegoDados.java
@@ -1,4 +1,23 @@
 package Controlador;
 
+import Modelo.Dado;
+
 public class JuegoDados {
+
+    public int sumarDados(int dado1, int dado2) {
+        return dado1 + dado2;
+    }
+    private void jugar(){
+    Dado dado1 = new Dado();
+    Dado dado2 = new Dado();
+        dado1.lanzarDado();
+        dado2.lanzarDado();
+    int resultado = sumarDados(dado1.getCaraVisible(), dado2.getCaraVisible());
+        if (resultado == 7) {
+        System.out.println("Has ganado!!!!!!!!!!!!!!!!!!!!!!!!");
+    } else {
+        System.out.println("Has perdido, bu");
+    }
 }
+}
+
