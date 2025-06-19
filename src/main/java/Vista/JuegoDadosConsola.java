4cd6544ae5ebca7939b074306c51aca58570de6d
patat <e.aguilera04@ufromail.cl>
2025-06-19 08:56:12 -0400

    Se implemento un menú basico para el futuro ingreso de opciones nuevas

--- src/main/java/Vista/JuegoDadosConsola.java
+++ src/main/java/Vista/JuegoDadosConsola.java
@@ -1,4 +1,34 @@
 package Vista;
 
+import java.util.Scanner;
+
 public class JuegoDadosConsola {
+    private static Scanner sc = new Scanner(System.in);
+
+    public void menu() {
+        int opcion;
+        do {
+            mostrarOpciones();
+            opcion = obtenerOpcion(0);
+            ejecutarOpcion(opcion);
+        } while (opcion != 2);
+    }
+
+    public void mostrarOpciones(int opcion) {
+        switch (opcion) {
+            case 1 -> System.out.println("1. Lanzar dados");
+            case 2 -> System.out.println("2. Salir");
+            default -> System.out.println("Opcion invalida");
+        }
+    }
+
+    private int obtenerOpcion(int opcion) {
+        try {
+            opcion = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Ingrese un numero valido");
+            return -1; // Devolver un valor inválido para que no ejecute ninguna opción
+        }
+        return opcion;
+    }
 }
