428d83af51f076435f7984da4471bde83fda915c
cristobal <c.ramos07@ufromail.cl>
2025-06-19 08:42:49 -0400

    clase dado completa

--- src/main/java/Modelo/Dado.java
+++ src/main/java/Modelo/Dado.java
@@ -1,4 +1,11 @@
 package Modelo;
 
 public class Dado {
+    private int caraVisible;
+    public void lanzarDado() {
+        this.caraVisible = (int)(Math.random()*6+1);
+    }
+    public int getCaraVisible() {
+        return caraVisible;
+    }
 }
